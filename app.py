851a0bec73b787d079dbe37e4ddaecdbd3d89d38
Marek Salat <salat.marek42@gmail.com>
2015-09-19 13:55:59 +0200

    dataset, app.py a matting.py

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
+from virtualcam.simple_matting.matting import AlphaMatte, ImageAligner
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
 __author__ = 'Marek'
 
 
-import cv2
-from matplotlib import pyplot as plt
+def plot(image_, index):
+    plt.subplot(2, 2, index)
+    plt.imshow(cv2.cvtColor(image_, cv2.COLOR_BGR2RGB), interpolation="quadric")
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 
 
-image = cv2.imread(r"dataset\IMAG0002.jpg", cv2.IMREAD_COLOR)
+def blend_images(_image, __image, alpha):
+    return cv2.addWeighted(_image, alpha, __image, 1 - alpha, 0)
 
-plt.imshow(image)
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+
+image = cv2.imread(r"dataset\IMAG0001.jpg", cv2.IMREAD_COLOR)
+control_image = cv2.imread(r"dataset\IMAG0002.jpg", cv2.IMREAD_COLOR)
+
+matting = AlphaMatte(image, control_image)
+trimap = matting.get_trimap()
+
+plot(image, 1)
+plot(control_image, 2)
+# plot(trimap, 3)
+control_image_transformed = ImageAligner(image, control_image).get_second_image()
+plot(blend_images(image, control_image_transformed, 0.5), 3)
+
+
+plt.subplot(2, 2, 4)
+plt.imshow(cv2.threshold(cv2.cvtColor(trimap, cv2.COLOR_BGR2GRAY), 32, 255, cv2.THRESH_BINARY)[1], 'gray')
+
 plt.show()
-
--- virtualcam/simple_matting/matting.py
+++ virtualcam/simple_matting/matting.py
@@ -13,19 +13,45 @@ class Trimap (Enum):
 
 
 class ImageAligner:
-    def __init__(self, first_image, second_image):
+    def __init__(self, first_image, second_image, iterations=100, termination_eps=1e-11):
         self.first_image = first_image
         self.second_image = second_image
+        self.warp_mode = cv2.MOTION_HOMOGRAPHY
+        self.warp_matrix = np.eye(3, 3, dtype=np.float32)
+        self.warp_matrix_calculated = False
+        self.number_of_iterations = iterations
+        self.termination_eps = termination_eps
+        self.criteria = (
+            cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
+            self.number_of_iterations,
+            self.termination_eps)
 
-    def get_first_image(self, cropped=True):
-        return self.first_image
+    def calculate_warp_matrix(self):
+        if not self.warp_matrix_calculated:
+            return self.warp_matrix
 
-    def get_second_image(self, cropped=True):
-        return self.second_image
+        (cc, self.warp_matrix) = cv2.findTransformECC(
+            cv2.cvtColor(self.first_image, cv2.COLOR_BGR2GRAY),
+            cv2.cvtColor(self.second_image, cv2.COLOR_BGR2GRAY),
+            self.warp_matrix,
+            self.warp_mode,
+            self.criteria
+        )
+        self.warp_matrix_calculated = True
+
+        return self.warp_matrix
+
+    def get_second_image(self):
+        shape = self.second_image.shape
+        return cv2.warpPerspective(
+            self.second_image,
+            self.calculate_warp_matrix(),
+            (shape[1], shape[0]),
+            flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
 
 
-class AlphaMate:
-    def __init__(self, image, control_image, input_trimap, image_aligner=None):
+class AlphaMatte:
+    def __init__(self, image, control_image, input_trimap=None, image_aligner=None):
         self.input_trimap = input_trimap
         self.control_image = control_image
         self.image = image
@@ -41,7 +67,10 @@ class AlphaMate:
         pass
 
     def get_trimap(self):
-        return self.trimap
+        image_converted = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+        control_image_converted = cv2.cvtColor(self.control_image, cv2.COLOR_BGR2HSV)
+        result = cv2.absdiff(image_converted, control_image_converted)
+        return cv2.cvtColor(result, cv2.COLOR_HSV2BGR)
 
     def _get_trimap(self):
         if self.trimap:
